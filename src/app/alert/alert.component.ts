95f45f91cf35e010b5a5d68eaf06a1a2fd383b76
anupkrbid <anupbid94@gmail.com>
2018-08-02 18:32:42 +0530

    animating leave

--- src/app/alert/alert.component.ts
+++ src/app/alert/alert.component.ts
@@ -14,21 +14,21 @@ const generateRandomId = () => {
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.css'],
   animations: [
-    trigger('listAnimaiton', [
+    trigger('alertAnimaiton', [
       transition('* => *', [
         query(':enter', style({ opacity: 0 }), { optional: true }),
         query(':enter', stagger('300ms', [
-          animate('1s ease-in', keyframes([
-            style({ opacity: 0, transform: 'translateX(-75px)', offset: 0 }),
-            style({ opacity: 0.5, transform: 'translateX(35px)', offset: 0.3 }),
+          animate('1s ease-out', keyframes([
+            style({ opacity: 0, transform: 'translateX(75px)', offset: 0 }),
+            style({ opacity: 1, transform: 'translateX(-35px)', offset: 0.3 }),
             style({ opacity: 1, transform: 'translateX(0)', offset: 1 })
           ]))
         ]), { optional: true }),
         query(':leave', stagger('300ms', [
           animate('1s ease-in', keyframes([
             style({ opacity: 1, transform: 'translateX(0)', offset: 0 }),
-            style({ opacity: 0.5, transform: 'translateX(35px)', offset: 0.3 }),
-            style({ opacity: 0, transform: 'translateX(-75px)', offset: 1 })
+            style({ opacity: 1, transform: 'translateX(-35px)', offset: 0.3 }),
+            style({ opacity: 0, transform: 'translateX(75px)', offset: 1 })
           ]))
         ]), { optional: true })
       ])
