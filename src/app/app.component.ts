05ec4a266b1a5274ecee818af4aeaeb0b192043b
Anupkumar Bid <anupkumar.bid@learningmatekol.local>
2018-08-02 15:26:58 +0530

    animaiton v1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { animate, group, keyframes, query, stagger, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('listAnimaiton', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), { optional: true }),
+        query(':enter', stagger('300ms', [
+          animate('1s ease-in', keyframes([
+            style({ opacity: 0, transform: 'translateY(-75px)', offset: 0 }),
+            style({ opacity: 0.5, transform: 'translateY(35px)', offset: 0.3 }),
+            style({ opacity: 1, transform: 'translateY(0)', offset: 1 })
+          ]))
+        ]), { optional: true }),
+        query(':leave', stagger('300ms', [
+          animate('1s ease-in', keyframes([
+            style({ opacity: 1, transform: 'translateY(0)', offset: 0 }),
+            style({ opacity: 0.5, transform: 'translateY(35px)', offset: 0.3 }),
+            style({ opacity: 0, transform: 'translateY(-75px)', offset: 1 })
+          ]))
+        ]), { optional: true })
+      ])
+    ]),
+    trigger('explainerAnimaiton', [
+      transition('* => *', [
+        query('.col', style({ opacity: 0, transform: 'translateX(-40px)' })),
+        query('.col', stagger('500ms', [
+          animate('800ms 1.2s ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
+        ]))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'app';
+  items = ['Hey this is an item.', 'Here is another one.', 'This is awesome.'];
+
+  addItem(item) {
+    this.items.push('new item');
+  }
+
+  removeItem(item) {
+    this.items.pop();
+  }
 }
